e7b65ad8641d8e928b89913fc3d903d67f83fba2
Diego Martins <diegomartins836@gmail.com>
2020-10-31 18:56:43 -0300

    Inserido moviventação da cobrinha

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let cobrinha = [];
+
+// variaveis de direçoes do jogo
+
+let direction = "right";
+
 cobrinha[0]={
     x: 8 * box,
     y: 8 * box
@@ -14,10 +19,44 @@ function criarBG(){
 
 function criaCobrinha(){
     for(i=0; i < cobrinha.length; i++){
-        context.fillStyle = "#ff7f50";
+        context.fillStyle = "#F4A460";
         context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
     }
 }
 
-criarBG();
-criaCobrinha();
+document.addEventListener("keydown",update); 
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame(){
+    if(cobrinha[0].x > 15 * box && direction == "right") cobrinha[0].x = 0;
+    if(cobrinha[0].x < 0 && direction == "left") cobrinha[0].x = 16 * box;
+    if(cobrinha[0].y > 15 * box && direction == "down") cobrinha[0].y = 0;
+    if(cobrinha[0].y < 0 && direction == "up") cobrinha[0].y = 16 * box;
+
+    criarBG();
+    criaCobrinha();
+    
+    let cobrinhaX = cobrinha[0].x;
+    let cobrinhaY = cobrinha[0].y;
+
+    if(direction == "right") cobrinhaX += box;
+    if(direction == "left") cobrinhaX -= box;
+    if(direction == "up") cobrinhaY -= box;
+    if(direction == "down") cobrinhaY += box;
+
+    cobrinha.pop();
+    let newHead={
+        x: cobrinhaX,
+        y: cobrinhaY
+    }
+
+    cobrinha.unshift(newHead);
+}
+
+let game = setInterval(startGame,100);
